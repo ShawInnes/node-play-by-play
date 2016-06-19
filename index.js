76b7b230d41dbf767f2780a1e028aef8e1da4571
Shaw Innes <shaw.innes@tattsgroup.com>
2016-06-19 11:13:09 +1000

    add bodyparser

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var http = require('http');
 var express = require('express');
-
 var app = express();
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function(req, res) {
   res.json({message: 'Hello World'});
